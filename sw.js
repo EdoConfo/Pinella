b4c361fc81b9f02f107e89a6a8f8dc400fe17b9b
EdoConfo <edo.conforti@stud.uniroma3.it>
2026-06-29 11:09:58 +0200

    feat: add contact icons to credits sheet

--- sw.js
+++ sw.js
@@ -5,14 +5,14 @@
    - Same-origin assets (versioned with ?v=): cache-first.
    - Cross-origin (Google Fonts): straight to network; CSS has system fallbacks offline.
    Bump VERSION (and the ?v= in ASSETS / index.html) whenever assets change. */
-var VERSION = "95";
+var VERSION = "96";
 var CACHE = "pinella-" + VERSION;
 var ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./assets/styles.css?v=55",
-  "./assets/js/main.js?v=95",
+  "./assets/styles.css?v=56",
+  "./assets/js/main.js?v=96",
   "./assets/img/icon-192.png",
   "./assets/img/icon-512.png",
   "./assets/img/apple-touch-icon.png"
